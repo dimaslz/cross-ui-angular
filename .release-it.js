cb36dd190c295d532d8f0cb8c41c7fa43ad3d37d
dimaslz <me@dimaslz.io>
2021-07-10 01:11:03 +0200

    chore: release v0.0.1-alpha.7

--- .release-it.js
+++ .release-it.js
@@ -9,7 +9,7 @@ module.exports = {
 	"hooks": {
     // "before:init": ["npm run lint", "npm test"],
     // "after:my-plugin:bump": "./bin/my-script.sh",
-    "after:bump": "NODE_ENV=production yarn build angular",
+    "after:bump": "NODE_ENV=production yarn build:components",
     "after:git:release": "echo After git push, before github release",
     "after:release": "echo Successfully released ${name} v${version} to ${repo.repository}."
   },
